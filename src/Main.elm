5d7050bff2e290b83b89f9a5299187a75e8268f7
cmarcbs7 <cmarbs7@gmail.com>
2019-08-05 21:16:29 -0500

    Implemented view

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,12 @@ module Main exposing (main)
 
 import Array exposing (Array)
 import Browser
-import Html exposing (Html)
+import Css
+import Css.Global
+import Html exposing (Html, Attribute)
+import Html.Attributes
+import Html.Styled
+import Html.Styled.Attributes as Style
 import Random exposing (Generator)
 import Time
 
@@ -12,26 +17,33 @@ import Time
 ------------------------------------------------------------------------------
 
 {-| DOCS MISSING -}
-fieldCols = 20
+fieldCols = 46
 
 {-| DOCS MISSING -}
-fieldRows = 10
+fieldRows = 50
 
 {-| DOCS MISSING -}
-liveP = 15
+sideLen = "10px"
 
 {-| DOCS MISSING -}
-deadP = 15
+liveP = 20
 
 {-| DOCS MISSING -}
-tickInterval = 1000
+deadP = 80
+
+{-| DOCS MISSING -}
+tickInterval = 125
 
 ------------------------------------------------------------------------------
--- Model
+-- Convenience Aliases
 ------------------------------------------------------------------------------
 
 {-| Zero-indexed coordinate reference for a two-dimensional grid -}
 type alias Coord = (Int, Int)
+
+------------------------------------------------------------------------------
+-- Model
+------------------------------------------------------------------------------
 
 {-| DOCS MISSING -}
 type CellState = Dead | Live
@@ -51,6 +63,8 @@ type alias Model =
     , rows : Int
     , vals : CellGrid
     }
+
+---- Instantiation -------------------
 
 {-| DOCS MISSING -}
 initModel : Model
@@ -76,44 +90,6 @@ cell col row state =
     }
 
 {-| DOCS MISSING -}
-get : Model -> Coord -> Maybe CellData
-get model (col, row) =
-    case Array.get col model.vals of
-        Just colVals ->
-            Array.get row colVals
-        _ ->
-            Nothing
-
-{-| DOCS MISSING -}
-getState : Model -> Coord -> Maybe CellState
-getState model coord =
-    case get model coord of
-        Just cellData -> Just cellData.state
-        _ -> Nothing
-
-{-| DOCS MISSING -}
-isDead : Model -> Bool
-isDead model =
-    let
-        accumulator val acc =
-            case val.state of
-                Dead -> True && acc
-                Live -> False
-        singleFold array =
-            Array.foldr accumulator True array
-    in
-        Array.foldr (\array acc -> acc && (singleFold array)) True model.vals
-
-{-| DOCS MISSING -}
-seedGen : Generator (List CellState)
-seedGen =
-    let
-        listLen = fieldCols * fieldRows
-        rWeight = Random.weighted (liveP, Live) [ (deadP, Dead) ]
-    in
-        Random.list listLen rWeight
-
-{-| DOCS MISSING -}
 gridFromSeed : List CellState -> CellGrid
 gridFromSeed ls =
     let
@@ -133,6 +109,50 @@ gridFromSeed ls =
     in
         Array.indexedMap rowIndex unindexed
 
+---- Random Generation ---------------
+
+{-| DOCS MISSING -}
+seedGen : Generator (List CellState)
+seedGen =
+    let
+        listLen = fieldCols * fieldRows
+        rWeight = Random.weighted (liveP, Live) [ (deadP, Dead) ]
+    in
+        Random.list listLen rWeight
+
+---- Value Retrieval -----------------
+
+{-| DOCS MISSING -}
+get : Model -> Coord -> Maybe CellData
+get model (col, row) =
+    case Array.get col model.vals of
+        Just colVals ->
+            Array.get row colVals
+        _ ->
+            Nothing
+
+{-| DOCS MISSING -}
+getState : Model -> Coord -> Maybe CellState
+getState model coord =
+    case get model coord of
+        Just cellData -> Just cellData.state
+        _ -> Nothing
+
+---- Boolean Analysis ----------------
+
+{-| DOCS MISSING -}
+isDead : Model -> Bool
+isDead model =
+    let
+        accumulator val acc =
+            case val.state of
+                Dead -> True && acc
+                Live -> False
+        singleFold array =
+            Array.foldr accumulator True array
+    in
+        Array.foldr (\array acc -> acc && (singleFold array)) True model.vals
+
 ------------------------------------------------------------------------------
 -- Update
 ------------------------------------------------------------------------------
@@ -142,6 +162,8 @@ type Msg
     = NewSim (List CellState)
     | Tick
     | Nil
+
+---- Determining Cell States ---------
 
 {-| Wrap coordinates around the field -}
 wrap : Model -> Coord -> Coord
@@ -204,6 +226,8 @@ nextState model coord =
             _ ->
                 Err "Coordinate out of bounds"
 
+---- Controller ----------------------
+
 {-| DOCS MISSING -}
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -248,9 +272,181 @@ subscriptions model =
 ------------------------------------------------------------------------------
 
 {-| DOCS MISSING -}
-view : Model -> Html Msg
+viewHeader : Html msg
+viewHeader =
+    let
+        attributes =
+            [ Html.Attributes.class "header"
+            , Html.Attributes.style "background-color" "#F4E8C1"
+            , Html.Attributes.style "border-bottom" "solid black"
+            , Html.Attributes.style "padding" "10px 0px 10px 0px"
+            , Html.Attributes.style "text-align" "center"
+            , Html.Attributes.style "position" "absolute"
+            , Html.Attributes.style "left" "0px"
+            , Html.Attributes.style "top" "0px"
+            , Html.Attributes.style "width" "100%"
+            ]
+    in
+        Html.div attributes
+            [ Html.h1
+                [ Html.Attributes.style "margin" "0px" ]
+                [ Html.text "Conway's Game of Life" ]
+            ]
+
+{-| DOCS MISSING -}
+viewSimRegion : Model -> Html msg
+viewSimRegion model =
+    let
+        attributes =
+            [ Html.Attributes.class "sim-region"
+            , Html.Attributes.style "border" "solid black"
+            , Html.Attributes.style "margin" "auto"
+            , Html.Attributes.style "margin-top" "107px"
+            , Html.Attributes.style "width" "598px"
+            ]
+    in
+        Html.div attributes
+            [ viewSimGrid model
+            , viewInfoPane model
+            ]
+
+{-| DOCS MISSING -}
+viewSimGrid : Model -> Html msg
+viewSimGrid model =
+    let
+        grid n =
+            if n > 0 then
+                (viewColumn model n)::(grid (n - 1))
+            else
+                []
+        attributes =
+            [ Html.Attributes.class "sim-grid"
+            , Html.Attributes.style "margin" "auto"
+            , Html.Attributes.style "line-height" "0px"
+            ]
+    in
+        Html.div attributes (grid fieldCols)
+
+{-| DOCS MISSING -}
+viewColumn : Model -> Int -> Html msg
+viewColumn model cInd =
+    let
+        column n =
+            if n > 0 then
+                (viewCell model (cInd - 1, n - 1))::(column (n - 1))
+            else
+                []
+        attributes =
+            [ Html.Attributes.class ("col-" ++ (String.fromInt cInd))
+            , Html.Attributes.style "display" "inline-block"
+            ]
+    in
+        Html.div attributes (column fieldRows)
+
+{-| DOCS MISSING -}
+viewCell : Model -> Coord -> Html msg
+viewCell model (col, row) =
+    let
+        state =
+            case getState model (col, row) of
+                Just Live ->
+                    Html.Attributes.class "live-cell"
+                _ ->
+                    Html.Attributes.class "dead-cell"
+        color =
+            case getState model (col, row) of
+                Just Live ->
+                    Html.Attributes.style "background-color" "#8075FF"
+                _ ->
+                    Html.Attributes.style "background-color" "#DDF8E8"
+        attributes =
+            [ Html.Attributes.class "cell"
+            , Html.Attributes.class ("row-" ++ (String.fromInt row))
+            , Html.Attributes.style "border" "solid black"
+            , Html.Attributes.style "height" sideLen
+            , Html.Attributes.style "width" sideLen
+            ]
+    in
+        Html.div (state::color::attributes)
+            []
+
+{-| DOCS MISSING -}
+viewLiveCell : Html msg
+viewLiveCell =
+    let
+        attributes =
+            [ Html.Attributes.class "live-cell"
+            ]
+    in
+        Html.div attributes
+            []
+
+{-| DOCS MISSING -}
+viewDeadCell : Html msg
+viewDeadCell =
+    let
+        attributes =
+            [ Html.Attributes.class "dead-cell"
+            ]
+    in
+        Html.div attributes
+            []
+
+{-| DOCS MISSING -}
+viewInfoPane : Model -> Html msg
+viewInfoPane model =
+    let
+        attributes =
+            [ Html.Attributes.class "info-pane"
+            ]
+    in
+        Html.div attributes
+            []
+
+{-| DOCS MISSING -}
+viewFooter : Html msg
+viewFooter =
+    let
+        attributes =
+            [ Html.Attributes.class "footer"
+            , Html.Attributes.style "background-color" "#F4E8C1"
+            , Html.Attributes.style "bottom" "0px"
+            , Html.Attributes.style "left" "0px"
+            , Html.Attributes.style "position" "fixed"
+            , Html.Attributes.style "padding" "5px"
+            , Html.Attributes.style "text-align" "center"
+            , Html.Attributes.style "font-style" "italic"
+            , Html.Attributes.style "font-size" "10pt"
+            , Html.Attributes.style "width" "100%"
+            , Html.Attributes.style "border-top" "solid black"
+            ]
+    in
+        Html.div attributes
+            [ Html.text "Conway's Game of Life, written in Elm - © 2019" ]
+
+{-| DOCS MISSING -}
+view : Model -> Html msg
 view model =
-    Debug.todo "view()"
+    let
+        global =
+            Css.Global.global
+                [ Css.Global.body
+                    [ Css.backgroundColor (Css.rgb 155 222 172)
+                    , Css.margin (Css.px 0)
+                    ]
+                ]
+            |> Html.Styled.toUnstyled
+        attributes =
+            [ Html.Attributes.class "container"
+            , Html.Attributes.style "padding" "10px"
+            ]
+    in
+        Html.div attributes
+            [ global
+            , viewHeader
+            , viewSimRegion model
+            , viewFooter
+            ]
 
 ------------------------------------------------------------------------------
 -- Entry Point
